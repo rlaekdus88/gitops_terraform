e0bb03a484fb52d085ae994573db0225da67fe2f
rlaekdus88 <rlaekdus88@gmail.com>
2026-05-07 21:35:01 +0900

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -12,7 +12,7 @@ module "eks" {
 
   eks_managed_node_group_defaults = {
     ami_type       = "AL2023_x86_64_STANDARD"
-    instance_types = ["t3.micro"]
+    instance_types = ["t3.medium"]
   }
 
   eks_managed_node_groups = var.node_groups
