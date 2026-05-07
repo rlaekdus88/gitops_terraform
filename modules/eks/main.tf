7c1fcaece302d499ea8c3c135caf156e35dd0808
rlaekdus88 <rlaekdus88@gmail.com>
2026-05-07 20:21:04 +0900

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,6 +1,6 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 19.0"
+  version = "~> 19.5"
 
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
@@ -11,8 +11,8 @@ module "eks" {
   cluster_endpoint_public_access = true
 
   eks_managed_node_group_defaults = {
-    ami_type       = "AL2_x86_64"
-    instance_types = ["t3.medium"]
+    ami_type       = "AL2023_x86_64_STANDARD"
+    instance_types = ["t3.micro"]
   }
 
   eks_managed_node_groups = var.node_groups
